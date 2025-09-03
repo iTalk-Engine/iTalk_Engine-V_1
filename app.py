20c49b27b047d1b7829c4afbbe2526de9f12e6b8
Jallow_2693 <229832439+iTalkEngine@users.noreply.github.com>
2025-09-03 11:42:08 +0000

    Update app.py

--- app.py
+++ app.py
@@ -92,4 +92,4 @@ def handle_send_message(data):
 
 # --- Lancement du serveur ---
 if __name__ == "__main__":
-    socketio.run(app, host="0.0.0.0", port=25568, debug=True)
+    socketio.run(app, host="0.0.0.0", port=5000, debug=True)
